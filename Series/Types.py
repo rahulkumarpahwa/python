cd88060824b1214a613c202d8bd3d5d34f30517b
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-01-19 11:21:46 +0530

    type method and typecasting

---
+++ Series/Types.py
@@ -0,0 +1,21 @@
+# Type Method
+a =31
+print(type(a))
+b="Apple"
+print(type(b))
+c=56.66
+print(type(c))
+d= None
+print(type(d))
+e=True
+print(type(e))
+
+print("\n")
+
+# Typecasting
+h = str(31) # 31 is type casted to string from the integer
+print(type(h))
+h = int (h) # type casted to integer
+print(type(h))
+h=float(h) # type casted to float
+print(h)
