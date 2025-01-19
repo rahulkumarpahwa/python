aaf2de4632b0160f4d1efedfff6157fc400e973d
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-01-19 12:59:29 +0530

    Average and Square

---
+++ Series/ch-2/Average.py
@@ -0,0 +1,5 @@
+a = int(input("Enter Ist Number "))
+b = int(input("Enter 2nd Number "))
+print("1st Number: ", a)
+print("2nd Number: ", b)
+print("Average of the two numbers is : ", (a+b)/2) # () is must
---
+++ Series/ch-2/Square.py
@@ -0,0 +1,3 @@
+a = int(input("Enter Any Number "))
+print("Number: ", a)
+print("Square of the number is : ", a*a)
