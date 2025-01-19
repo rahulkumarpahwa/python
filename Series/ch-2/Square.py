8e10bb2edc5fa926e18301c2a8c142dc29d777e3
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-01-19 13:01:08 +0530

    part added in square

--- Series/ch-2/Square.py
+++ Series/ch-2/Square.py
@@ -1,3 +1,4 @@
 a = int(input("Enter Any Number "))
 print("Number: ", a)
 print("Square of the number is : ", a*a)
+print("Square of the number is : ", a**2) # ** is operator to find the nth power of the number.
