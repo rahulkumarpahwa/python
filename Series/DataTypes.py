b9d2a12868e7073bfe070ca81eb85ceb1b6a2e47
rahulkumarpahwa <rahulkumarpahwa@gmail.com>
2025-01-19 09:17:23 +0530

    datatypes and operators

---
+++ Series/DataTypes.py
@@ -0,0 +1,57 @@
+# Data Types
+a = 23 #integer 
+b = 45.44 # float
+c= "Namaste World!" # String
+bool = True # boolean
+empty = None #None datatype 
+
+# Operators:
+
+# 1. Arithmetic Operators:
+a =34
+b= 36
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+
+print("\n")
+
+# 2. Comparison Operator:
+d= a>b 
+print(d)
+d= a>=b 
+print(d)
+d= a<b 
+print(d)
+d= a<=b 
+print(d)
+d= a==b 
+print(d)
+d= a!=b 
+print(d)
+
+print("\n")
+
+# 3. Assignment Operators:
+a+=2
+print(a)
+a-=2
+print(a)
+a*=2
+print(a)
+a/=2
+print(a)
+a%=2
+print(a)
+
+print("\n")
+
+# 4. Logical Operators
+d= True or False
+print(d)
+d= True and False
+print(d)
+d= not(False)
+print(d)
